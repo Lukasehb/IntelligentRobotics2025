249473d5547b4ac16f8cd6c8eecd5f2ef30ae2b8
lukasdevroey <Lukas.devroey@student.ehb.be>
2025-10-23 11:33:24 +0200

    added batterycheck

--- arduino/Opencr/classContent/classContent.ino
+++ arduino/Opencr/classContent/classContent.ino
@@ -2,32 +2,59 @@
 #include "songs.h"
 #include "sensors.h"
 
+int criticalnote = NOTE_A4;
+float criticalVoltage = 11.5;
+unsigned long lastBatteryCheck = 0; 
+
 void setup() {
   Serial.begin(115200);
   Serial.println("Launching IMU");
   IMU.begin();
   Serial.println("Playing start melody");
-  playSequence(DarthVader,noteDurations_DarthVader);
-  pinMode( led_pin, OUTPUT );
+  playSequence(DarthVader, noteDurations_DarthVader);
+  pinMode(led_pin, OUTPUT);
+}
+
+float checkCriticalBattery() {
+  float voltage = getBatteryVoltage();
+  Serial.println("battery check");
+
+  while (voltage < criticalVoltage) {
+    tone(BDPIN_BUZZER, NOTE_A4, 100);
+    Serial.println("Battery critical ");
+    float voltage = getBatteryVoltage();
+    delay(1000);
+
+  }
+
+  return false;
 }
 
 void loop() {
 
-    float roll = getRoll();
-    float yaw = getYaw();
-    float pitch = getPitch();
+  float roll = getRoll();
+  float yaw = getYaw();
+  float pitch = getPitch();
 
-    float voltage = getBatteryVoltage();
-    
-    Serial.println(voltage);
+  bool criticalVoltageBool = false;
+  float voltage = getBatteryVoltage();
 
-    Serial.print("Roll:");
-    Serial.print(roll);
-    Serial.print(",Pitch:");
-    Serial.print(pitch);
-    Serial.print(",Yaw:");
-    Serial.println(yaw);
-    delay(100);  
+  if(millis() - lastBatteryCheck >= 60000 )
+  {
+
+    lastBatteryCheck = millis();
+    criticalVoltageBool = checkCriticalBattery();
+
+  }
 
 
+  Serial.print("Battery voltage: ");
+  Serial.print(voltage);
+  Serial.print(" ,Roll: ");
+  Serial.print(roll);
+  Serial.print(",Pitch: ");
+  Serial.print(pitch);
+  Serial.print(" ,Yaw: ");
+  Serial.println(yaw);
+  delay(100);
 }
